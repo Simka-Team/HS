3acd63b265e36970c9e1636588b4aef4863a9e8b
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2021-03-21 15:46:32 +0300

    new

--- alexhs.py
+++ alexhs.py
@@ -1,1 +1,18 @@
-print("HS")
+import random
+
+
+print(random.randint(0, 1))
+
+# create test bots
+# card 1 = 5/7
+# card 2 = 3/7
+
+first_my_attack = 5
+first_my_hp = 7
+first_enemy_attack = 3
+first_enem
+
+
+def attack
+
+
