76f0aae02ed089cfc5c45c2355e5568ba8670e1b
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2021-03-22 20:42:15 +0300

    start count card

--- alexhs.py
+++ alexhs.py
@@ -1,8 +1,8 @@
 import random
 
 
-print(random.randint(0, 1))
-
+who_start_turn = random.randint(0, 1)
+c = 0
 # create test bots
 # card 1 = 5/7
 # card 2 = 3/7
@@ -10,9 +10,32 @@ print(random.randint(0, 1))
 first_my_attack = 5
 first_my_hp = 7
 first_enemy_attack = 3
-first_enem
+first_enemy_hp = 7
+first_my_card = [first_my_attack, first_my_hp]
+first_enemy_card = [first_enemy_attack, first_enemy_hp]
+all_cards = []
+def count_cards(list):
+    if list(0) + list(1) > 0:
+        all_cards.append(list)
 
 
-def attack
+
+
+
+
+def attack(first_my_attack, first_my_hp, first_enemy_attack_first_enemy_hp):
+    while c < 15:
+       c + 1
+        if c == 1:
+            count_cards(first_my_card)
+    #if who_start_turn == 0:
+  #     while:
+
+
+
+
+
+
+
 
 
