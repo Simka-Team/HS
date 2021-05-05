53a9dbfbfe694b5cbf8e97380058b9c307087b8c
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2021-05-05 18:46:42 +0300

    Update alexhs.py

--- alexhs.py
+++ alexhs.py
@@ -17,7 +17,7 @@ first_my_card = [first_my_attack, first_my_hp, first_my_team]
 first_enemy_card = [first_enemy_attack, first_enemy_hp, first_enemy_team]
 all_my_cards = []
 all_enemy_cards = []
-ab = ''
+ab = []
 
 
 
@@ -45,14 +45,11 @@ def attack(all_my_cards, all_enemy_cards):
     ab = all_my_cards[a], all_enemy_cards[b]
     return ab
 
-print(attack(all_my_cards, all_enemy_cards))
-
-
-
-
-
-#random.randint(0, )
-
+zxc = attack(all_my_cards, all_enemy_cards)
+all_my_cards = zxc[0]
+all_enemy_cards = zxc[1]
+print(all_my_cards)
+print(all_enemy_cards)
 
 
 
