c93b5cf0d1cc46aeeda102f0381ecf57742b19bf
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2021-05-07 21:39:57 +0300

    Update alexhs.py
    просто добавил 2 переменные и перед и в print написал что за переменная из чего сделал вывод что все работает

--- alexhs.py
+++ alexhs.py
@@ -15,6 +15,14 @@ first_enemy_hp = 7
 first_enemy_team = 1
 first_my_card = [first_my_attack, first_my_hp, first_my_team]
 first_enemy_card = [first_enemy_attack, first_enemy_hp, first_enemy_team]
+second_my_attack = 5
+second_my_hp = 10
+second_my_team = 0
+second_my_card = [second_my_attack, second_my_hp, second_my_team]
+second_enemy_attack = 3
+second_enemy_hp = 15
+second_enemy_team = 1
+second_enemy_card = [second_enemy_attack, second_enemy_hp, second_enemy_team]
 all_my_cards = []
 all_enemy_cards = []
 ab = []
@@ -22,20 +30,19 @@ ab = []
 
 
 
-def count_cards(check_card):
+def count_cards(check_card, cards):
    if check_card[1] > 0:
-       if check_card[2] == 0:
-            all_my_cards.append(check_card)
-       elif check_card[2] == 1:
-           all_enemy_cards.append(check_card)
+           cards.append(check_card)
    else:
         pass
 
 
-count_cards(first_my_card)
-count_cards(first_enemy_card)
-print(all_my_cards)
-print(all_enemy_cards)
+count_cards(first_my_card, all_my_cards)
+count_cards(first_enemy_card, all_enemy_cards)
+count_cards(second_my_card, all_my_cards)
+count_cards(second_enemy_card, all_enemy_cards)
+print(f'{all_my_cards} мои карты')
+print(f'{all_enemy_cards} карты врага')
 
 def attack(all_my_cards, all_enemy_cards):
     a = random.randint(0 ,len(all_my_cards)-1)
@@ -48,9 +55,5 @@ def attack(all_my_cards, all_enemy_cards):
 zxc = attack(all_my_cards, all_enemy_cards)
 all_my_cards = zxc[0]
 all_enemy_cards = zxc[1]
-print(all_my_cards)
-print(all_enemy_cards)
-
-
-
-
+print(f'{all_my_cards} первая рандомная карта которая ударилась')
+print(f'{all_enemy_cards} вторая карта которая ударилась')
