5d32e777d53842b29bff112aedd6243dc84f19b4
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2021-05-10 16:15:40 +0300

    Update alexhs.py

--- alexhs.py
+++ alexhs.py
@@ -1,32 +1,52 @@
 import random
 
 
-who_start_turn = random.randint(0, 1)
-c = 0
-# create test bots
-# card 1 = 5/7
-# card 2 = 3/7
 
-first_my_attack = 5
-first_my_hp = 7
+
+#who_start_turn = random.randint(0, 1)
+c = 0
+firstnum = 0
+# create test bots
+# card my 1  = 5/7
+# card en 1 = 3/7
+# card my 2 = 5/10
+# card en 2 = 3/15
+
+
+# card my 1  = 5/7
+first_my_attack = random.randint(1,30)
+first_my_hp = random.randint(20,30)
 first_my_team = 0
-first_enemy_attack = 3
-first_enemy_hp = 7
-first_enemy_team = 1
 first_my_card = [first_my_attack, first_my_hp, first_my_team]
+
+
+# card en 1 = 3/7
+first_enemy_attack = random.randint(1,30)
+first_enemy_hp = random.randint(20,30)
+first_enemy_team = 1
 first_enemy_card = [first_enemy_attack, first_enemy_hp, first_enemy_team]
-second_my_attack = 5
-second_my_hp = 10
+
+
+# card my 2 = 5/10
+second_my_attack = random.randint(1,30)
+second_my_hp = random.randint(20,30)
 second_my_team = 0
 second_my_card = [second_my_attack, second_my_hp, second_my_team]
-second_enemy_attack = 3
-second_enemy_hp = 15
+
+
+# card en 2 = 3/15
+second_enemy_attack = random.randint(1,30)
+second_enemy_hp = random.randint(20,30)
 second_enemy_team = 1
 second_enemy_card = [second_enemy_attack, second_enemy_hp, second_enemy_team]
+
+
+
 all_my_cards = []
 all_enemy_cards = []
 ab = []
-
+attack_all_my_cards = []
+attack_all_enemy_cards = []
 
 
 
@@ -36,11 +56,21 @@ def count_cards(check_card, cards):
    else:
         pass
 
+def clear_cards(cards):
+    new_cards = []
+    for i in range(len(cards)):
+       if cards[i][1] > 0:
+           new_cards.append(cards[i])
+    return new_cards
+
+
 
 count_cards(first_my_card, all_my_cards)
 count_cards(first_enemy_card, all_enemy_cards)
 count_cards(second_my_card, all_my_cards)
 count_cards(second_enemy_card, all_enemy_cards)
+attack_all_enemy_cards = all_enemy_cards
+attack_all_my_cards = all_my_cards
 print(f'{all_my_cards} мои карты')
 print(f'{all_enemy_cards} карты врага')
 
@@ -52,8 +82,40 @@ def attack(all_my_cards, all_enemy_cards):
     ab = all_my_cards[a], all_enemy_cards[b]
     return ab
 
+''' проверка атаки
 zxc = attack(all_my_cards, all_enemy_cards)
-all_my_cards = zxc[0]
-all_enemy_cards = zxc[1]
-print(f'{all_my_cards} первая рандомная карта которая ударилась')
-print(f'{all_enemy_cards} вторая карта которая ударилась')
+after_attack_my = zxc[0]
+after_attack_enemy = zxc[1]
+print(f'{after_attack_my} моя рандомная карта которая ударилась')
+print(f'{after_attack_enemy} вражеская карта которая ударилась')
+'''
+
+#attack(all_my_cards, all_enemy_cards)
+#print(all_my_cards)
+#print(all_enemy_cards)
+
+
+
+
+
+print(all_my_cards)
+attack_all_my_cards = clear_cards(all_my_cards)
+print(attack_all_my_cards)
+i = 0
+
+
+while len(attack_all_my_cards) * len(attack_all_enemy_cards) > 0:
+    attack(attack_all_my_cards, attack_all_enemy_cards)
+    attack_all_my_cards = clear_cards(attack_all_my_cards)
+    attack_all_enemy_cards = clear_cards(attack_all_enemy_cards)
+    print(f'{attack_all_my_cards} мои карты')
+    print(f'{attack_all_enemy_cards} карты врага')
+    i+=1
+    print(i)
+
+if len(attack_all_my_cards) + len(attack_all_enemy_cards) == 0:
+    print("draw")
+elif len(attack_all_my_cards) > len(attack_all_enemy_cards):
+    print('win')
+elif len(attack_all_my_cards) < len(attack_all_enemy_cards):
+    print("lose")
