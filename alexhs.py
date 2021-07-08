5b350d707b6d54529df3961ba4631ec08e02076f
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2021-07-08 17:23:06 +0300

    Update alexhs.py
    сделана атака, проверка для добавления и само добавление

--- alexhs.py
+++ alexhs.py
@@ -4,9 +4,13 @@ import random
 
 my_num_attack = 0
 enemy_num_attack = 0
-who_start_turn = random.randint(0, 1)
+who_start_turn = 2
 c = 0
 firstnum = 0
+temp_of_my = []
+temp_of_enemy = []
+died_my_cards = []
+died_enemy_cards = []
 # вариант
 var = 1
 temp = []
@@ -23,28 +27,32 @@ gap = 2
 first_my_attack = 5
 first_my_hp = 25
 first_my_team = 0
-first_my_card = [gap, first_my_team, first_my_attack, first_my_hp]
+first_my_reality = -1
+first_my_card = [gap, first_my_team, first_my_attack, first_my_hp, first_my_reality]
 
 
 # card en 1 = 3/7
 first_enemy_attack = 7
 first_enemy_hp = 30
 first_enemy_team = 1
-first_enemy_card = [gap, first_enemy_team, first_enemy_attack, first_enemy_hp]
+first_enemy_reality = -1
+first_enemy_card = [gap, first_enemy_team, first_enemy_attack, first_enemy_hp, first_enemy_reality]
 
 
 # card my 2 = 5/10
 second_my_attack = 12
 second_my_hp = 45
 second_my_team = 0
-second_my_card = [gap, second_my_team, second_my_attack, second_my_hp]
+second_my_reality = -1
+second_my_card = [gap, second_my_team, second_my_attack, second_my_hp, second_my_reality]
 
 
 # card en 2 = 3/15
 second_enemy_attack = 3
 second_enemy_hp = 50
 second_enemy_team = 1
-second_enemy_card = [gap, second_enemy_team, second_enemy_attack, second_enemy_hp]
+second_enemy_reality = -1
+second_enemy_card = [gap, second_enemy_team, second_enemy_attack, second_enemy_hp,second_enemy_reality]
 
 #card my 3 = 11/22
 #third_my_attack =
@@ -82,9 +90,21 @@ attack_all_my_cards = all_my_cards
 
 # print(f'{all_my_cards} мои карты')
 # print(f'{all_enemy_cards} карты врага')
+def who_start_turn(my_cards, enemy_cards):
+    global who_start_turn
+    if len(my_cards) > len(enemy_cards):
+        who_start_turn = 0
+    elif len(my_cards) < len(enemy_cards):
+        who_start_turn = 1
+    else:
+        who_start_turn = random.randint(0, 1)
+    return who_start_turn
 
+
+# сделано
 def attack(attack_cards, def_cards ,num_attack):
-    global way, temp
+    global way, temp, temp_of_attack, temp_of_def, who_start_turn
+
     var = 0
     if way == 0:
         i = 0
@@ -102,16 +122,50 @@ def attack(attack_cards, def_cards ,num_attack):
             else:
                 attack_cards[attack_card][0] = 0
                 def_cards[def_cards][0] = 0
+
             way += 1
             var += 1
 
-def check_for_add(def_cards, attack_cards):
-    global
-def adding(def_cards, attack_cards):
+    return attack_cards, def_cards
+# сделано
+def check_for_add(attack_cards, def_cards):
+    temp_of_attack = []
+    temp_of_def = []
+    died_attack = []
+    died_def = []
+    if len(attack_cards) * len(def_cards) > 0:
+        temp_of_attack.append(attack_cards)
+        temp_of_def.append(def_cards)
+        return temp_of_attack, temp_of_def, 1
+    elif len(attack_cards) * len(def_cards) == 0:
+        died_attack.append(attack_cards)
+        died_def.append(def_cards)
+        return died_attack, died_def, 0
+# сделано
+def adding(attack_cards, def_cards, life):
+    global who_start_turn, temp_of_my, temp_of_enemy, died_my_cards, died_enemy_cards
+    if life == 1:
+        if who_start_turn == 0:
+            temp_of_my.append(attack_cards)
+            temp_of_enemy.append(def_cards)
+        elif who_start_turn == 1:
+            temp_of_my.append(def_cards)
+            temp_of_enemy.append(attack_cards)
+    elif life == 0:
+        if who_start_turn == 0:
+            died_my_cards.append(attack_cards)
+            died_enemy_cards.append(def_cards)
+        elif who_start_turn == 1:
+            died_my_cards.append(def_cards)
+            died_enemy_cards.append(attack_cards)
+        
+            
 
-    temp.append(str(def_cards))
-    temp.append(str(attack_cards))
-    list(temp)
+
+
+    #temp.append(str(def_cards))
+   # temp.append(str(attack_cards))
+   # list(temp)
 #            temp[len(temp) - 1] = l
             #            ist(temp[len(temp) - 1])
  #           temp[def_len] = list(temp[def_len])
@@ -168,20 +222,13 @@ def attack(attack_cards, def_cards, num_attack):
 #            temp[len(temp) - 1] = list(temp[len(temp) - 1])
             temp.append(str(attack_cards))
             list(temp)
-
-
 #            temp[len(temp) - 1] = list(temp[len(temp) - 1])
             way+=1
             print(var)
             print(temp)
-
-
             var+=1
     num_attack+=1
     var = 0
-
-
-
 '''
 ''' проверка атаки
 zxc = attack(all_my_cards, all_enemy_cards)
@@ -209,7 +256,6 @@ while len(attack_all_my_cards) * len(attack_all_enemy_cards) > 0:
         attack(attack_all_my_cards, attack_all_enemy_cards, my_num_attack)
     elif len(attack_all_enemy_cards) > len(attack_all_my_cards):
         attack(all_enemy_cards, attack_all_my_cards, enemy_num_attack)
-
     else:
         if who_start_turn == 0:
             attack(attack_all_my_cards, attack_all_enemy_cards, my_num_attack)
@@ -221,7 +267,6 @@ while len(attack_all_my_cards) * len(attack_all_enemy_cards) > 0:
     print(f'{attack_all_enemy_cards} карты врага')
     i+=1
     print(i)
-
 if len(attack_all_my_cards) + len(attack_all_enemy_cards) == 0:
     print("draw")
 elif len(attack_all_my_cards) > len(attack_all_enemy_cards):
@@ -229,4 +274,4 @@ elif len(attack_all_my_cards) > len(attack_all_enemy_cards):
 elif len(attack_all_my_cards) < len(attack_all_enemy_cards):
     print("lose")
 '''
-attack(all_my_cards, all_enemy_cards, my_num_attack)
+print(attack(all_my_cards, all_enemy_cards, my_num_attack))
